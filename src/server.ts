5baf76c1f7eab87af20c959573ce7216a0cc2a8b
Christopher Torres <k.torres@seznam.cz>
2026-04-18 09:56:22 +0200

    Added rejection and exception handling to Server

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,14 @@ const server = app.listen(env.PORT, () => {
 
 // TODO
 // Unhandled promise rejection
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled rejection:', reason)
+  server.close(() => process.exit(1))
+})
 
 // TODO
 // Unhandled unchanght exceptions
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught exception:', error)
+  server.close(() => process.exit(1))
+})
