79b1e82decfba3248e4c479c23cb2744e87eb786
Christopher Torres <73254689+torres-christopher@users.noreply.github.com>
2026-04-21 13:11:03 +0000

    Pocet znaku schema and services

--- src/modules/core/text/pocet-znaku/pocet-znaku.service.ts
+++ src/modules/core/text/pocet-znaku/pocet-znaku.service.ts
@@ -2,25 +2,57 @@ import { type pocetZnakuInput, type pocetZnakuOutput } from './pocet-znaku.schem
 
 export const calculatePocetZnaku = function(input: pocetZnakuInput): pocetZnakuOutput {
     const outputObject: pocetZnakuOutput = {
-        textLength: calculateTextLength(input.text),
+        textLengthRaw: calculateTextLengthRaw(input.text),
+        textLengthNoSpace: calculateTextLengthNoSpace(input.text),
         wordCount: calculateWordCount(input.text),
         nsCount: calculateNsCount(input.text),
+        lineCount: calculateLineCount(input.text),
+        sentenceCount: calculateSentenceCount(input.text),
+        readingTime: calculateReadingTime(input.text),
     }
     return outputObject
 }
 
-// Text length
-const calculateTextLength = function(input: string):number {
+// Text length raw
+const calculateTextLengthRaw = function(input: string):number {
     return input.length
+}
+
+// Text length without spaces
+const calculateTextLengthNoSpace = function(input: string):number {
+    return input.replace(/\s/g, '').length
 }
 
 // Word amount
 const calculateWordCount = function(input: string): number {
-    return input.split(" ").length
+    if (isWhitespaceString(input)) return 0
+    return input.trim().split(/\s+/).length
 }
 
-// NS length
+// Sentence count
+const calculateSentenceCount = function(input: string):number {
+    if (isWhitespaceString(input)) return 0
+    const sentenceCount = input.match(/[\w)][.?!](\s|$)/g) 
+    return !sentenceCount ? 0 : sentenceCount.length
+}
+
+// NS length with space
 const calculateNsCount = function(input: string):number {
-    const textLength: number = calculateTextLength(input)
+    const textLength: number = calculateTextLengthRaw(input)
     return textLength/1800
 }
+
+// Line count
+const calculateLineCount = function(input: string):number {
+    if (isWhitespaceString(input)) return 0
+    return input.trim().split(/\r\n|\r|\n/).length
+}
+
+// Reading time rounded upwards
+const calculateReadingTime = function(input: string):number {
+    const wordCount = calculateWordCount(input)
+    return Math.ceil(wordCount/200)
+}
+
+// Check if string is just whitespace
+const isWhitespaceString = (input: string): boolean => !input.replace(/\s/g, '').length
