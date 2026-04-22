0ba0a572ba023b42546e7784599d619503e58fe7
Christopher Torres <k.torres@seznam.cz>
2026-04-22 22:59:20 +0200

    Removed empty test

--- src/modules/text/pocet-znaku/pocet-znaku.routes.test.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getPocetZnaku, postPocetZnaku } from './pocet-znaku.controller.js'
-
-const router = express.Router()
-
-router.get('/pocet-znaku', getPocetZnaku)
-router.post('/pocet-znaku', postPocetZnaku)
-
-export default router
