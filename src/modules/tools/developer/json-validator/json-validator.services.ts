2d805518a801400b6e5a7e16256f1ddf98c6e0a5
Christopher Torres <73254689+torres-christopher@users.noreply.github.com>
2026-04-25 10:37:46 +0000

    Added comments and first draft of error position

--- src/modules/tools/developer/json-validator/json-validator.services.ts
+++ src/modules/tools/developer/json-validator/json-validator.services.ts
@@ -1,8 +1,12 @@
 import type { JsonValidatorInput, JsonValidatorOutput } from './json-validator.schema.js'
 
 export const jsonValidateFormat = function (input: JsonValidatorInput): JsonValidatorOutput {
+  // Always check JSON validity
   const valid = validateJSON(input.text)
+
+  // Format only if JSON valid
   if (valid.valid) {
+    // Default in case of cust validation
     let result: string = input.text
     switch (input.actionType) {
       case 'format':
@@ -20,6 +24,10 @@ export const jsonValidateFormat = function (input: JsonValidatorInput): JsonVali
   } else {
     return {
       validJson: valid.valid,
+      errorPosition: {
+        line: valid.line,
+        column: valid.line,
+      },
     }
   }
 }
@@ -33,11 +41,13 @@ interface ValidateJson {
 // Validate
 const validateJSON = function (input: string): ValidateJson {
   try {
+    // Parser will throw syntax error if wrong JSON
     JSON.parse(input)
     return {
-      valid: true,
+      valid: true, // Always return true cause false would go to catch(error)
     }
   } catch (error) {
+    // Check for SyntaxError otherwise error is of type unknown
     if (error instanceof SyntaxError) {
       const line = error.message.split('line ')[1].split('column')[0]
       const column = error.message.split('column ')[1].split(')')[0]
@@ -47,6 +57,7 @@ const validateJSON = function (input: string): ValidateJson {
         column: column ? Number(column) : 0,
       }
     }
+    // Edge case for non-Syntax errors
     return {
       valid: false,
       line: 0,
@@ -55,12 +66,12 @@ const validateJSON = function (input: string): ValidateJson {
   }
 }
 
-// Format
+// Format JSON
 const formatJSON = function (input: string): string {
   return input
 }
 
-// Minify
+// Minify JSON
 const minifyJSON = function (input: string): string {
   return input
 }
