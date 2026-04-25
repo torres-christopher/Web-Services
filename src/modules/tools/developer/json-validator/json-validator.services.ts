a6d8d67471d7534fcd92351b485360b09f5e3763
Christopher Torres <73254689+torres-christopher@users.noreply.github.com>
2026-04-25 10:28:17 +0000

    Added JSON validation

--- src/modules/tools/developer/json-validator/json-validator.services.ts
+++ src/modules/tools/developer/json-validator/json-validator.services.ts
@@ -1,13 +1,66 @@
 import type { JsonValidatorInput, JsonValidatorOutput } from './json-validator.schema.js'
 
+export const jsonValidateFormat = function (input: JsonValidatorInput): JsonValidatorOutput {
+  const valid = validateJSON(input.text)
+  if (valid.valid) {
+    let result: string = input.text
+    switch (input.actionType) {
+      case 'format':
+        result = formatJSON(input.text)
+        break
+      case 'minify':
+        result = minifyJSON(input.text)
+        break
+    }
+
+    return {
+      validJson: valid.valid,
+      result,
+    }
+  } else {
+    return {
+      validJson: valid.valid,
+    }
+  }
+}
+
+interface ValidateJson {
+  valid: boolean
+  line?: number
+  column?: number
+}
+
 // Validate
-export const validateJSON = function (input: JsonValidatorInput): JsonValidatorOutput {
-  const test = input.Text
-  return {
-    validJson: false,
+const validateJSON = function (input: string): ValidateJson {
+  try {
+    JSON.parse(input)
+    return {
+      valid: true,
+    }
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      const line = error.message.split('line ')[1].split('column')[0]
+      const column = error.message.split('column ')[1].split(')')[0]
+      return {
+        valid: false,
+        line: line ? Number(line) : 0,
+        column: column ? Number(column) : 0,
+      }
+    }
+    return {
+      valid: false,
+      line: 0,
+      column: 0,
+    }
   }
 }
 
 // Format
+const formatJSON = function (input: string): string {
+  return input
+}
 
 // Minify
+const minifyJSON = function (input: string): string {
+  return input
+}
